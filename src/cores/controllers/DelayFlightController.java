88d8ff3b8304a0d528d406584edd680f0fc08db2
Redson2202 <fbarriosa@uninorte.edu.co>
2025-05-24 14:41:49 -0500

    falta un controlador :p

---
+++ src/cores/controllers/DelayFlightController.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cores.controllers;
+
+import cores.utils.Response;
+import cores.utils.Status;
+import cores.models.Flight;
+import cores.models.storage.Storage;
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author User
+ */
+public class DelayFlightController {
+    public static Response Delayflight(String flightid, int delayhours,int delayminutes){
+        Storage storage = Storage.getInstance();
+        //Validando que el id no este vacio
+        if(flightid == null || flightid.isEmpty()){
+            return new Response("Id no valido",Status.BAD_REQUEST);
+        }
+        //Validar que las horas/minutos de retraso sean >0
+        if(delayhours<0||delayminutes<0||(delayhours==0 & delayminutes ==0)){
+            return new Response("El tiempo de retraso debe ser mayor de 00:00",Status.BAD_REQUEST);
+        }
+        //Validar que el vuelo exista
+        Flight flight = storage.getFlightById(flightid);
+        if(flight == null){
+            return new Response("Vuelo no encontrado",Status.NOT_FOUND);
+        }
+        LocalDateTime orignalDeparture = flight.getDepartureDate();
+        Duration delay = Duration.ofHours(delayhours).plusMinutes(delayminutes);
+        LocalDateTime newDeparture = orignalDeparture.plus(delay);
+        flight.setDepartureDate(newDeparture);
+        return new Response("Vuelo retrasado correctamente",Status.OK);
+        
+    }
+}
---
+++ src/cores/controllers/UpdatePassenger.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cores.controllers;
+
+import cores.models.Passenger;
+import cores.models.storage.Storage;
+import cores.utils.Response;
+import cores.utils.Status;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author User
+ */
+public class UpdatePassenger {
+
+    public static Response updatePassenger(Passenger updatedPassenger, long PassengerId) {
+        Storage storage = Storage.getInstance();
+        Passenger passenger = storage.getPassengerById(PassengerId);
+
+        //Validando si el pasajero existe/valido
+        if (passenger == null) {
+            return new Response("Pasajero no encontrado", Status.NOT_FOUND);
+        }
+        //Validando que el nombre no este vacio
+        if (updatedPassenger.getFirstname() == null || updatedPassenger.getLastname() == null) {
+            return new Response("El nombre/Apellido no pueden estar vacios",Status.BAD_REQUEST);
+        }
+        Passenger existingpassenger = storage.getPassengerById(updatedPassenger.getId());
+        //Actualizar la informacion
+        existingpassenger.setFirstname(updatedPassenger.getFirstname());
+        existingpassenger.setBirthDate(updatedPassenger.getBirthDate());
+        existingpassenger.setLastname(updatedPassenger.getLastname());
+        existingpassenger.setCountry(updatedPassenger.getCountry());
+        existingpassenger.setPhone(updatedPassenger.getPhone());
+        existingpassenger.setCountryPhoneCode(updatedPassenger.getCountryPhoneCode());
+        return new Response("Informacion actualizada de manera exitosa",Status.OK);
+    }
+}
